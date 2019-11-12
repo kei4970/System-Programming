86cc49e76ed2f4357a5a9e592d01a34f3f0dd3ff
kei4970 <55121356+kei4970@users.noreply.github.com>
2019-11-12 10:47:25 +0900

    Add files via upload

---
+++ System-Programming/1112/fork2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+int main(){
+	int pid;
+	pid = fork();
+	if(pid==0){
+		printf("[Child]:Hello, World pid =%d\n",getpid());
+	}
+	else{
+		printf("[Parent]:Hello, World pid=%d\n",getpid());
+	}
+}
+
